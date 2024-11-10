15f28016ba24b8df6f96ef87a5d78c8bca41aa2c
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-11-10 15:12:12 +0200

    Update middleware to include TMDB watchlist API route and enhance route matching

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-    publicRoutes: ["/"]
+  publicRoutes: ["/", "/api/tmdb/watchlist"],
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+    "/api/:path*",
+  ],
 };
